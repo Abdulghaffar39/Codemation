99ed0a361ccfc2b0c55b8ce85948128c965cbc8a
abdulghaffar39 <abdulghaffar15a@gmail.com>
2026-07-11 03:43:08 -0700

    work complete

--- infra/packer/scripts/provision.sh
+++ infra/packer/scripts/provision.sh
@@ -4,7 +4,9 @@
 set -euo pipefail
 
 sudo apt-get update -y
-sudo apt-get install -y curl nginx jq awscli
+sudo apt-get install -y curl nginx jq awsc
+
+li
 
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt-get install -y nodejs
